0d5ed97b6da7cbf9966a28f1cad5e7b408512392
Vo1dling <attlanticazz@hotmail.com>
2021-12-18 14:45:24 +0200

    fixed an icons issues and changed the dice images

--- src/components/Dice/Dice.components.jsx
+++ src/components/Dice/Dice.components.jsx
@@ -1,34 +1,44 @@
 import React from "react";
 import "./Dice.styles.css";
+import throwImage from "./assets/img/throw.png";
+import holdImage from "./assets/img/hold.png";
 class Dice extends React.Component {
-	state = { dice1: 0, dice2: 0, diceSum: 0 };
-	RollDice = () => {
-		const dice1 = Math.floor(Math.random() * 6) + 1;
-		const dice2 = Math.floor(Math.random() * 6) + 1;
-		if (dice1 === 6 && dice2 === 6) {
-			this.setState({ dice1: dice1, dice2: dice2, diceSum: 0 }, this.AssignDice);
-		} else {
-			this.setState({ dice1: dice1, dice2: dice2, diceSum: dice1 + dice2 }, this.AssignDice);
-		}
-	};
-	AssignDice = () => {
-		document.querySelector("#dice1").className = `number${this.state.dice1}`;
-		document.querySelector("#dice2").className = `number${this.state.dice2}`;
-		this.props.onChange(this.state.diceSum);
-	};
-	render() {
-		return (
-			<div className="dice-container">
-					<div id="dice1"></div>
-					<div id="dice2"></div>
-				<button type="button" onClick={this.RollDice}>
-					<span className="throw-img">asdasdd</span> Throw
-				</button>
-				<button type="button" onClick={this.props.onClick} className="hold-btn">
-				    <span className="hold-img">aa</span> Hold
-				</button>
-			</div>
-		);
-	}
+  state = { dice1: 0, dice2: 0, diceSum: 0 };
+  RollDice = () => {
+    if (!this.props.gameOver) {
+      const dice1 = Math.floor(Math.random() * 6) + 1;
+      const dice2 = Math.floor(Math.random() * 6) + 1;
+      if (dice1 === 6 && dice2 === 6) {
+        this.setState(
+          { dice1: dice1, dice2: dice2, diceSum: 0 },
+          this.AssignDice
+        );
+      } else {
+        this.setState(
+          { dice1: dice1, dice2: dice2, diceSum: dice1 + dice2 },
+          this.AssignDice
+        );
+      }
+    }
+  };
+  AssignDice = () => {
+    document.querySelector("#dice1").className = `number${this.state.dice1}`;
+    document.querySelector("#dice2").className = `number${this.state.dice2}`;
+    this.props.onChange(this.state.diceSum);
+  };
+  render() {
+    return (
+      <div className="dice-container">
+        <div id="dice1"></div>
+        <div id="dice2"></div>
+        <button type="button" onClick={this.RollDice}>
+          <img src={throwImage} alt="Throw Image" /> Throw
+        </button>
+        <button type="button" onClick={this.props.onClick} className="hold-btn">
+          <img src={holdImage} alt="Hold Image" /> Hold
+        </button>
+      </div>
+    );
+  }
 }
 export default Dice;
--- src/components/Player/Player.components.jsx
+++ src/components/Player/Player.components.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 import Counter from "../Counter/Counter.components";
 const Player = (props) => {
-	const { globalScore, tempScore, playerNumber } = props;
-	return (
-		<div className={`player-${playerNumber}`}>
-			<h2>Player-{playerNumber}</h2>
-			<Counter playerNumber={playerNumber} isTemp={false} score={globalScore} />
-			<Counter playerNumber={playerNumber} isTemp={true} score={tempScore} />
-		</div>
-	);
+  const { globalScore, tempScore, playerNumber } = props;
+  return (
+    <div className={`player-${playerNumber}`}>
+      <div className="text-container">
+        <h2>Player-{playerNumber}</h2>
+        <Counter
+          playerNumber={playerNumber}
+          isTemp={false}
+          score={globalScore}
+        />
+        <Counter playerNumber={playerNumber} isTemp={true} score={tempScore} />
+      </div>
+      <div className="winner-text display-none">
+        <h3>Winner!!</h3>
+      </div>
+    </div>
+  );
 };
 export default Player;
-
